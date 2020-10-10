f63c786f8bb2775abc7b5714732b6002559698df
VanDerJoys <52749354+VanDerJoys@users.noreply.github.com>
2020-10-11 00:36:17 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+      <router-view></router-view>
+    </div>
+</template>
+
+<script>
+  import jQuery from 'jquery';
+  window.jQuery = jQuery;
+
+  export default {
+    name: 'App',
+    mounted(){
+      this.$nextTick(function(){
+        jQuery('.sidenav').sidenav();
+        jQuery('.tooltipped').tooltip();
+        jQuery('.dropdown-trigger').dropdown({'constrainWidth': false, 'coverTrigger' : false, 'alignment':'right'});
+      })
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
