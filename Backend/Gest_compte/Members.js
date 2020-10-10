04d3da08ee849ad13e0d81004aa89b899ab888b1
VanDerJoys <52749354+VanDerJoys@users.noreply.github.com>
2020-10-11 00:45:36 +0100

    Add files via upload

--- Backend/Gest_compte/Members.js
+++ Backend/Gest_compte/Members.js
@@ -1,14 +1,24 @@
 const Model = require('../Modèle/model');
 
 class Members{
-    async getMembers(idEnt){
+    async getMembers(idEmp){
         const db = new Model();
-        let members = await db.getMembers(idEnt).then((results)=>{
+        let members = await db.getMembers(idEmp).then((results)=>{
             return results;
         }).catch((error)=>{
             return error;
         });
         return members;
+    }
+
+    async getName(idEmp){
+        const db = new Model();
+        let name = await db.getName(idEmp).then((results)=>{
+            return results;
+        }).catch((error)=>{
+            return error;
+        })
+        return name;
     }
 }
 
