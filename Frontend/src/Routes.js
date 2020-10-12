2bdab682f52e9d7e6153fa6d807d9731692662f4
VanDerJoys <52749354+VanDerJoys@users.noreply.github.com>
2020-10-12 23:56:33 +0100

    Add files via upload

--- Frontend/src/Routes.js
+++ Frontend/src/Routes.js
@@ -1,9 +1,11 @@
 import Login from './components/Login.vue'
 import Workbox from './components/WorkBox.vue'
 import Inscription from './components/Inscription.vue'
+import Réunions from './components/Réunions'
 
 export default[
     {path : '/', component : Inscription},
     {path : '/Login', component : Login},
-    {path : '/Workbox', component : Workbox}
+    {path : '/Workbox/:id', component : Workbox},
+    {path : '/Réunions', component : Réunions}
 ]
