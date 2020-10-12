bc1c22b70bdb191ac2f7a4e54d596d9e86309856
VanDerJoys <52749354+VanDerJoys@users.noreply.github.com>
2020-10-12 23:53:47 +0100

    Add files via upload

--- Backend/Gest_horaire/date_heure.js
+++ Backend/Gest_horaire/date_heure.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 
 class DateHour{
     getDateHour(){
-        moment.locale();
+        moment.locale('fr');
         let date = moment().format("L");
         let heure = moment().format("LTS")
         return {date, heure};
