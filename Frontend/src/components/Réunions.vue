11ac54f9328760588a1b14a937d9698cbe134eb5
VanDerJoys <bissa.mintyene@gmail.com>
2020-11-06 13:41:28 +0100

    add .gitignore

--- Frontend/src/components/Réunions.vue
+++ Frontend/src/components/Réunions.vue
@@ -9,71 +9,23 @@
 </template>
 
 <script>
-import axios from 'axios';
-import Peer from 'peerjs';
-import io from 'socket.io-client'
+import io from 'socket.io-client';
+import SimplePeer from 'simple-peer';
+
 export default {
     name : "réunions",
     data(){
       return {
         socket : io('localhost:3000'),
-        peers : {},
-        peer : new Peer()
+        peer : new SimplePeer({
+          initiator : true,
+          stream : false
+        })
       }
     },
     methods: {
-      async startVideo(){
-        let uuid = await axios.get(`http://localhost:3000/Reunions`);
-        document.querySelector('.container').remove();
-        const myVideo = document.createElement('video');
-          navigator.mediaDevices.getUserMedia({
-            video: true
-          }).then(stream => {
-            this.addVideoStream(myVideo, stream)
-
-            this.peer.on('call', call => {
-              console.log('dedans1');
-              call.answer(stream)
-              const video = document.createElement('video')
-              call.on('stream', userVideoStream => {
-                this.addVideoStream(video, userVideoStream)
-              })
-            })
-
-            this.socket.on('user-connected', userId => {
-              console.log('dedans2');
-              this.connectToNewUser(userId, stream)
-            })
-          })
-
-          this.socket.on('user-disconnected', userId => {
-            if (this.peers[userId]) this.peers[userId].close()
-          })
-          
-          this.peer.on('open', id => {
-            console.log('dedans3');
-            this.socket.emit('join-room', uuid.data, id)
-          })
-      },
-      connectToNewUser(userId, stream) {
-        const call = this.peer.call(userId, stream)
-        const video = document.createElement('video')
-        call.on('stream', userVideoStream => {
-          this.addVideoStream(video, userVideoStream)
-        })
-        call.on('close', () => {
-          video.remove()
-        })
-
-        this.peers[userId] = call
-      },
-      addVideoStream(video, stream) {
-        let videoGrid = document.getElementById('video-grid');
-        video.srcObject = stream
-        video.addEventListener('loadedmetadata', () => {
-          video.play()
-        })
-        videoGrid.append(video)
+      startVideo(){
+        
       }
     }
 }
